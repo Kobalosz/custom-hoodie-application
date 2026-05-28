dc91b6d7982a78d0c75d1599f080ab08e3848cfb
Kobalosz <imanuel.dartey@gmail.com>
2026-05-27 17:49:50 -0700

    Update: wire up application in Main.java

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,9 +1,12 @@
 package com.pluralsight;
 
+import com.pluralsight.views.HomeScreen;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-
+        HomeScreen app = new HomeScreen();
+        app.start();
     }
 }
